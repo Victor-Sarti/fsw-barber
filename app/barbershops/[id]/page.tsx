900a81b97d930d88562a3891754f01b621a03af0
victor-sarti <sartivictor24@gmail.com>
2024-08-26 21:07:32 -0300

    create id new pag barber

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/app/_lib/prisma";
+
+
+interface BarbershopPageProps{
+    params: {
+        id: string
+    }
+}
+
+const BarbershopPage = async ({params}: BarbershopPageProps) => {
+    //chamar o banco 
+    const barbershop = await db.barbershop.findUnique ({
+            where: {
+                id: params.id,
+            }
+    })
+    
+    
+    return ( <h1> {barbershop?.name} </h1> );
+}
+ 
+export default BarbershopPage;
